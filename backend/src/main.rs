fdca7e0ef9dd5811b65ad8e806edc6849c635e8e
Tomislav Ljubej <tomislavl@tbtl.com>
2023-03-18 15:18:03 +0100

    Add warp helloworld to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([0, 0, 0, 0], 3030)).await;
 }
